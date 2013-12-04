3f54c9a75f7798a1db5b8ac22a6ba341c9edeb32
Christian Haack <ragamo@gmail.com>
2013-12-04 17:35:41 -0300

    v.0.0.3

--- admin/config/database.php
+++ admin/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'flocsflh_dev';
-$db['default']['password'] = 'c{Be%RB$KuTW';
-$db['default']['database'] = 'flocsflh_devpy';
+$db['default']['username'] = 'username';
+$db['default']['password'] = 'password';
+$db['default']['database'] = 'database';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
