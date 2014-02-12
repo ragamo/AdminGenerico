e95a9c0646b200bb2b862f1c01a7db92c792c108
Christian Haack <ragamo@gmail.com>
2014-02-12 12:05:51 -0300

    Bugfix: Se encriptan las cookies por defecto.

--- admin/config/config.php
+++ admin/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'a89{MsJk12!';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
